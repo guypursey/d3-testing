4636ec2a20819bd64e052b7990bf1f59d8a1dee6
Guy Pursey <guypursey@gmail.com>
2016-05-02 18:17:08 +0100

    Added initial code to repo.

---
+++ serve.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+var fs = require('fs')
+
+app.use(express.static('.'))
+
+app.get('/', function (req, res) {
+  var content = fs.readFileSync('index.html', 'utf8')
+  res.send(content)
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
