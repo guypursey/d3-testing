21ab31f1b5523d8947338507bd5faf8624ada291
Guy Pursey <guypursey@gmail.com>
2016-05-30 22:24:26 +0100

    Changed serve file to allow viewing of various expermients.
    Defaults to original ranged bar chart view but currently
    by going to `/timeline` you can see the latest experiment.

--- serve.js
+++ serve.js
@@ -5,6 +5,11 @@ var fs = require('fs')
 app.use(express.static('.'))
 
 app.get('/', function (req, res) {
+  var content = fs.readFileSync('ranged-bar-chart.html', 'utf8')
+  res.send(content)
+})
+
+app.get('/timeline', function (req, res) {
   var content = fs.readFileSync('timeline.html', 'utf8')
   res.send(content)
 })
