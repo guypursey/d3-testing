dcd2c117e334f83a08a17543e262740c174cc4e8
Guy Pursey <guypursey@gmail.com>
2016-08-14 18:35:05 +0100

    Changed server to point to local directories for files.

--- serve.js
+++ serve.js
@@ -2,40 +2,45 @@ var express = require('express')
 var app = express()
 var fs = require('fs')
 
-app.use(express.static('.'))
-
 app.get('/', function (req, res) {
   var content = fs.readFileSync('journal-entries-ranged-bar-chart/index.html', 'utf8')
+  app.use(express.static('./journal-entries-ranged-bar-chart'))
   res.send(content)
 })
 
 app.get('/tweaked-bunkat-timeline', function (req, res) {
   var content = fs.readFileSync('tweaked-bunkat-timeline/index.html', 'utf8')
+  app.use(express.static('./tweaked-bunkat-timeline'))
   res.send(content)
 })
 
 app.get('/playpool-chart', function (req, res) {
   var content = fs.readFileSync('playpool-chart/index.html', 'utf8')
+  app.use(express.static('./playpool-chart'))
   res.send(content)
 })
 
 app.get('/eu-ref', function (req, res) {
   var content = fs.readFileSync('uk-eu-referendum-hierarchical-bar-chart/index.html', 'utf8')
+  app.use(express.static('./uk-eu-referendum-hierarchical-bar-chart'))
   res.send(content)
 })
 
 app.get('/eu-arc', function (req, res) {
   var content = fs.readFileSync('eu-referendum-arc-results-1.html', 'utf8')
+  app.use(express.static('.'))
   res.send(content)
 })
 
 app.get('/eu-arc-2', function (req, res) {
   var content = fs.readFileSync('eu-referendum-arc-results-2.html', 'utf8')
+  app.use(express.static('.'))
   res.send(content)
 })
 
 app.get('/eu-sun', function (req, res) {
   var content = fs.readFileSync('eu-sunburst.html', 'utf8')
+  app.use(express.static('.'))
   res.send(content)
 })
 
